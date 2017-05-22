9fa81190c7ed42a21b56a3fa53cbbd4d96074b65
tsaezb <tomas.saezbin@gmail.com>
2017-05-22 10:51:54 -0400

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name infoBoxApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the infoBoxApp
+ */
+angular.module('infoBoxApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name infoBoxApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the infoBoxApp
+ */
+angular.module('infoBoxApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
