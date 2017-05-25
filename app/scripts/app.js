50f55925cd26598f851103082a48fdfcd3705034
tsaezb <tomas.saezbin@gmail.com>
2017-05-24 22:39:03 -0400

    add bootstrap, add about-us, and modify home

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,7 +13,9 @@ angular
     'ngAnimate',
     'ngResource',
     'ngRoute',
-    'ngSanitize'
+    'ngSanitize',
+    'ngTouch',
+    'ui.bootstrap'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -21,11 +23,6 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,29 @@
  * Controller of the infoBoxApp
  */
 angular.module('infoBoxApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $uibModal) {
+
+
+    $scope.contact_info = [
+      {
+        "header": "Developer",
+        "data": "Tomás Sáez Binelli"
+      },{
+        "header": "Email",
+        "data": "tomas.saezbin@gmail.com"
+      },{
+        "header": "Teacher",
+        "data": "Aidan Hogan"
+      }
+    ]
+
+
+    $scope.open_contact_us = function(){
+      $uibModal.open({
+        templateUrl: 'views/contact-us.html',
+        scope: $scope,
+        size: 'md'
+      }).result.then(function(){}, function(res){});
+    }
+
   });
