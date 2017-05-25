1a2fa7612f1d9971a7fb2b86e9fe444d99f3768d
tsaezb <tomas.saezbin@gmail.com>
2017-05-24 23:27:18 -0400

    add entity id input

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,9 @@
  * Controller of the infoBoxApp
  */
 angular.module('infoBoxApp')
-  .controller('MainCtrl', function ($scope, $uibModal) {
+  .controller('MainCtrl', function ($scope, $uibModal){
 
+    $scope.regex = /^[\d]+$/;
 
     $scope.contact_info = [
       {
@@ -22,15 +23,22 @@ angular.module('infoBoxApp')
         "header": "Teacher",
         "data": "Aidan Hogan"
       }
-    ]
-
+    ];
 
     $scope.open_contact_us = function(){
       $uibModal.open({
         templateUrl: 'views/contact-us.html',
         scope: $scope,
         size: 'md'
-      }).result.then(function(){}, function(res){});
-    }
+      }).result.then(function(){}, function(r){})
+    };
+
+    $scope.search_entity_info = function(id){
+      if(regex.test(id)){
+        console.log(id);
+      }
+      else{
+      }
+    };
 
   });
