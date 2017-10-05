4b133b0b1f9ac0f56120182a29d7ca5fc36fbec1
tsaezb <tomas.saezbin@gmail.com>
2017-10-05 17:39:34 -0300

    api connection and delegation applied

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,6 +17,7 @@ angular.module('infoBoxApp')
 
     //add form for this field later
     $scope.lang = "en";
+    $scope.strategy = "baseline";
 
     $scope.addAlert = function(str) {
       if ($scope.alerts !== []) {
@@ -53,28 +54,25 @@ angular.module('infoBoxApp')
       }).result.then(function(){}, function(r){});
     };
 
-    $scope.get_entity_info = function(id, lang){
-      $scope.get_wikidata_info(id, lang);
+    $scope.get_entity_info = function(id, lang, strat){
+      $scope.get_wikidata_info(id, lang, strat);
     };
 
     //quering function
-    $scope.get_wikidata_info = function(id, lang){
+    $scope.get_wikidata_info = function(id, lang, strat){
 
       //$http.get("https://query.wikidata.org/bigdata/namespace/wdq/sparql?query=" + encodeURI(query))
       $http({
-        url:'http://localhost:8000/entity?id='+ id + '&lang=' + lang,
+        url:'http://localhost:8000/entity?id='+ id + '&lang=' + lang + '&strategy=' + strat,
         method: 'GET',
         withCredentials: false
       }).then(function(response){
           if(response.data.length !== 0){
             $scope.alerts = [];
             $scope.data_flag = true;
-            $scope.entity_data = response.data;
-
-            $scope.info_box = $scope.filter_properties(response.data, lang);
-
-            //random order & select first 10
-            $scope.info_box = $scope.baseline_infobox($scope.info_box);
+            $scope.entity_label = response.data.label;
+            $scope.entity_description = response.data.description;
+            $scope.info_box = response.data.properties;
           }
 
           else{
@@ -84,40 +82,4 @@ angular.module('infoBoxApp')
           $scope.addAlert("There was an error processing the query!");
         });
     };
-
-
-    //filter function
-    $scope.filter_properties = function(properties_list, lang){
-
-      var filtered_pairs = [];
-
-      for (var i = 0; i < properties_list.length; i++) {
-
-        //ignoring non-direct properties
-        if (properties_list[i].pLabel.value.indexOf("http://www.wikidata.org/prop/P") === 0) {}
-
-        //adding direct properties
-        else if (properties_list[i].pLabel.value.indexOf("http://www.wikidata.org/prop/direct") === 0) {
-          filtered_pairs.push(properties_list[i]);
-        }
-
-            //check for literals in given language
-        else if (properties_list[i].val.type === "literal"){
-
-          if (properties_list[i].val["xml:lang"] === lang || properties_list[i].datatype === "http://www.w3.org/2001/XMLSchema#integer" || properties_list[i].val["xml:lang"] === undefined){
-            filtered_pairs.push(properties_list[i]);
-          }
-        }
-      }
-
-      return filtered_pairs;
-    };
-
-    //this function creates the baseline infobox for each entity, selecting 10 properties at random
-    $scope.baseline_infobox = function(properties){
-      //add label as 1rst
-      return _.shuffle(properties).slice(0,10);
-    };
-
-
-    });
+  });
